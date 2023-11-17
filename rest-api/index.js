0e4a2482978a27faa85f57dcbd6f19e3cbad12b9
anurag <23anuragsharma@gmail.com>
2023-11-17 20:57:50 +0530

    Basic api

---
+++ rest-api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+
+const app = express();
+const PORT = 8000;
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
+
+app.post("/api/users", (req, res) => {
+  return res.json({ status: "pending" });
+});
+
+app.patch("/api/users/:id", (req, res) => {
+  return res.json({ status: "pending" });
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  return res.json({ status: "pending" });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port http://localhost:${PORT}`);
+});
