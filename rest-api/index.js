e1c98f3f965b1406534d43259a52ddba70f24a7d
anurag <23anuragsharma@gmail.com>
2023-11-18 08:12:22 +0530

    API

--- rest-api/index.js
+++ rest-api/index.js
@@ -18,11 +18,19 @@ app.get("/api/users/:id", (req, res) => {
 
 app.post("/api/users", (req, res) => {
   const body = req.body;
-  users.push({ id: users.length + 1, ...body });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "pending" });
+  
+  users.push({
+    id: users.length + 1,
+    ...body,
   });
-  console.log(body);
+ 
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({
+      status: "success",
+      id: users.length + 1,
+    });
+  });
+  // console.log(body);
 });
 
 app.patch("/api/users/:id", (req, res) => {
@@ -30,8 +38,11 @@ app.patch("/api/users/:id", (req, res) => {
 });
 
 app.delete("/api/users/:id", (req, res) => {
-  return res.json({ status: "pending" });
+  const id = parseInt(req.params.id); // Extract the ID from URL params
+  users.delete(id);
+  return res.json({ status: "success" });
 });
+
 
 app.listen(PORT, () => {
   console.log(`listening on port http://localhost:${PORT}`);
